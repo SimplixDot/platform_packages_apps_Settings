cc3f32e96d900075983dbd10a9892e96b958fec9
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-03 03:38:03 +0000

    Settings: Append Cosmic-OS build date/type on build display id
    Change-Id: I831800ae0a769513b6b0c008ba492cd8334b3b65
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -46,6 +46,8 @@ import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
 import com.android.settingslib.development.DevelopmentSettingsEnabler;
 
+import android.os.SystemProperties;
+
 public class BuildNumberPreferenceController extends AbstractPreferenceController implements
         PreferenceControllerMixin, LifecycleObserver, OnResume {
 
@@ -83,12 +85,25 @@ public class BuildNumberPreferenceController extends AbstractPreferenceControlle
         final Preference preference = screen.findPreference(KEY_BUILD_NUMBER);
         if (preference != null) {
             try {
-                preference.setSummary(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+                StringBuilder sb = new StringBuilder();
+                sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+                String CosmicVersion = getCosmicVersion();
+                if (!CosmicVersion.equals("")){
+                    sb.append("\n");
+                    sb.append(CosmicVersion);
+                }
+                preference.setSummary(sb.toString());
                 preference.setEnabled(true);
             } catch (Exception e) {
                 preference.setSummary(R.string.device_info_default);
             }
         }
+    }
+
+    private String getCosmicVersion(){
+        String buildDate = SystemProperties.get("ro.cos.build_date","");
+        String buildType = SystemProperties.get("ro.cos.releasetype","unofficial").toUpperCase();
+        return buildDate.equals("") ? "" : "Cosmic-OS-" + buildDate + "-" + buildType;
     }
 
     @Override
